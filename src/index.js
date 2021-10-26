b63c59a1ab5211abc4234468766f8a23d00c9396
karajrish <rishabh.karajgi@gmail.com>
2021-10-26 22:17:54 +0530

    pushed files

--- src/index.js
+++ src/index.js
@@ -1,60 +1,48 @@
 const { ApolloServer } = require('apollo-server')
 const fs = require('fs')
 const path = require('path')
+const { PrismaClient } = require('@prisma/client')
+const { getUserId } = require('./utils')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const { PubSub } = require('apollo-server')
+const Subscription = require('./resolvers/Subscription')
+const Vote = require('./resolvers/Vote')
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }]
 
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links,
-    },
-    Mutation: {
-        post: (parent, args) => {
-            let idCount = links.length
-                const link = {
-                    id: `link-${idCount++}`,
-                    description: args.description,
-                    url: args.url
-                }
-            links.push(link)
-            return link
-        },
-        update: (parent, args) => {
-            link = links.filter(obj => {
-                return args.id === obj.id;   
-                }).map(obj => {
-                    obj.description = args.description
-                    obj.url = args.url
-                })
-            return link
-
-        },
-        delete: (parent, args) => {
-            const index = links.findIndex(x => x.id === args.id);
-            const link = links[index]
-            if (index !== undefined) links.splice(index, 1);
-            return link
-        }
-    },
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url
-    }
+    Query,
+    Mutation,
+    Subscription,
+    User,
+    Link,
+    Vote
 }
+
+const prisma = new PrismaClient()
+
+const pubsub = new PubSub()
 
 const server = new ApolloServer({
     typeDefs: fs.readFileSync(
-        path.join(__dirname, 'schema.graphql'),
-        'utf8'
+      path.join(__dirname, 'schema.graphql'),
+      'utf8'
     ),
-    resolvers
-})
+    resolvers,
+    context: ({ req }) => {
+      return {
+        ...req,
+        prisma,
+        pubsub,
+        userId:
+          req && req.headers.authorization
+            ? getUserId(req)
+            : null
+      };
+    }
+  });
 
 server
     .listen()
---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,95 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { APP_SECRET, getUserId } = require('../utils')
+
+
+
+async function post(parent, args, context, info) {
+    const { userId } = context;
+  
+    const newLink = await context.prisma.link.create({
+      data: {
+        url: args.url,
+        description: args.description,
+        postedBy: { connect: { id: userId } },
+      }
+    })
+    context.pubsub.publish("NEW_LINK", newLink)
+    return newLink
+  }
+
+async function signup(parent, args, context, info) {
+    // 1
+    const password = await bcrypt.hash(args.password, 10)
+  
+    // 2
+    const user = await context.prisma.user.create({ data: { ...args, password } })
+  
+    // 3
+    const token = jwt.sign({ userId: user.id }, APP_SECRET)
+  
+    // 4
+    return {
+      token,
+      user,
+    }
+  }
+  
+  async function login(parent, args, context, info) {
+    // 1
+    const user = await context.prisma.user.findUnique({ where: { email: args.email } })
+    if (!user) {
+      throw new Error('No such user found')
+    }
+  
+    // 2
+    const valid = await bcrypt.compare(args.password, user.password)
+    if (!valid) {
+      throw new Error('Invalid password')
+    }
+  
+    const token = jwt.sign({ userId: user.id }, APP_SECRET)
+  
+    // 3
+    return {
+      token,
+      user,
+    }
+  }
+
+  async function vote(parent, args, context, info) {
+    // 1
+    const userId = context.userId
+  
+    // 2
+    const vote = await context.prisma.vote.findUnique({
+      where: {
+        linkId_userId: {
+          linkId: Number(args.linkId),
+          userId: userId
+        }
+      }
+    })
+  
+    if (Boolean(vote)) {
+      throw new Error(`Already voted for link: ${args.linkId}`)
+    }
+  
+    // 3
+    const newVote = context.prisma.vote.create({
+      data: {
+        user: { connect: { id: userId } },
+        link: { connect: { id: Number(args.linkId) } },
+      }
+    })
+    context.pubsub.publish("NEW_VOTE", newVote)
+  
+    return newVote
+  }
+  
+  module.exports = {
+    signup,
+    login,
+    post,
+    vote,
+  }
